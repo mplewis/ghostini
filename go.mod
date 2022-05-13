6699b730114211512efdd73a3c45273293a7afd5
Matt Lewis <matt@mplewis.com>
2022-05-12 18:43:56 -0600

    feat: working server with basic router

--- go.mod
+++ go.mod
@@ -2,9 +2,4 @@ module github.com/mplewis/ghostini
 
 go 1.18
 
-require github.com/PuerkitoBio/goquery v1.8.0
-
-require (
-	github.com/andybalholm/cascadia v1.3.1 // indirect
-	golang.org/x/net v0.0.0-20210916014120-12bc252f5db8 // indirect
-)
+require github.com/a-h/gemini v0.0.66
--- main.go
+++ main.go
@@ -1,10 +1,53 @@
 package main
 
 import (
+	"context"
+	"crypto/tls"
 	"fmt"
 	"log"
 	"os"
+	"regexp"
+	"strings"
+
+	"github.com/a-h/gemini"
 )
+
+type Server struct{}
+
+var allPostsMatcher = regexp.MustCompile(`^/posts/?$`)
+var slugMatcher = regexp.MustCompile(`^/posts/([^/]+)/?$`)
+
+// /posts => get page 1 of all posts
+// /posts?page=2 => get page 2 of all posts
+// /posts/some-blog-post-slug => get post with slug "some-blog-post-slug"
+func (s Server) ServeGemini(w gemini.ResponseWriter, r *gemini.Request) {
+	fmt.Println(r.URL.Path)
+
+	if r.URL.Path == "/" {
+		fmt.Println("home")
+		w.SetHeader(gemini.CodeRedirect, "/posts")
+		return
+	}
+
+	if allPostsMatcher.MatchString(r.URL.Path) {
+		fmt.Println("all posts")
+		w.SetHeader(gemini.CodeSuccess, "")
+		w.Write([]byte("all posts"))
+		return
+	}
+
+	if matches := slugMatcher.FindStringSubmatch(r.URL.Path); len(matches) > 0 {
+		slug := matches[1]
+		fmt.Printf("one post: %s\n", slug)
+		w.SetHeader(gemini.CodeSuccess, "")
+		w.Write([]byte(fmt.Sprintf("one post: %s", slug)))
+		return
+	}
+
+	fmt.Println("invalid path")
+	w.SetHeader(gemini.CodeNotFound, "")
+	w.Write([]byte("not found"))
+}
 
 func check(err error) {
 	if err != nil {
@@ -12,16 +55,40 @@ func check(err error) {
 	}
 }
 
-func main() {
-	h := host{
-		apiUrl:     "https://kesdev.com",
-		contentKey: os.Getenv("CONTENT_KEY"),
+func mustEnv(key string) string {
+	if val := os.Getenv(key); val != "" {
+		return val
 	}
-	c := newCache()
-	resp, err := getPosts(c, h, 1)
+	log.Fatalf("Missing mandatory environment variable %s", key)
+	return ""
+}
+
+var c = newCache()
+var h = host{
+	apiUrl:     mustEnv("GHOST_SITE"),
+	contentKey: mustEnv("CONTENT_KEY"),
+}
+
+func main() {
+	if !(strings.HasPrefix(h.apiUrl, "http://") || strings.HasPrefix(h.apiUrl, "https://")) {
+		log.Fatalf("GHOST_SITE must start with http:// or https://")
+	}
+	h.apiUrl = strings.TrimSuffix(h.apiUrl, "/")
+	fmt.Printf("Starting server for Ghost site at %s\n", h.apiUrl)
+
+	// resp, err := getPosts(c, h, 1)
+	// check(err)
+	// fmt.Println(resp)
+	// resp, err = getPosts(c, h, 1)
+	// check(err)
+	// fmt.Println(resp)
+
+	cert, err := tls.LoadX509KeyPair("tmp/localhost.crt", "tmp/localhost.key")
 	check(err)
-	fmt.Println(resp)
-	resp, err = getPosts(c, h, 1)
-	check(err)
-	fmt.Println(resp)
+
+	domain := gemini.NewDomainHandler("localhost", cert, Server{})
+	err = gemini.ListenAndServe(context.Background(), ":1965", domain)
+	if err != nil {
+		log.Fatal("error:", err)
+	}
 }
